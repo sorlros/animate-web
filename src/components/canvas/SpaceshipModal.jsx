c74ee3fd073a603c1c28b59d75a6268126126ca4
sorlros <sorlros2047@gmail.com>
2023-05-25 15:32:55 +0900

    애니메이션 효과 수정

--- src/components/canvas/SpaceshipModal.jsx
+++ src/components/canvas/SpaceshipModal.jsx
@@ -4,35 +4,37 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 import Loader from "../Loader";
 import * as THREE from "three";
 
-export const SpaceshipModal = ({ scale, position }) => {
-	const spaceship = useGLTF("./raven_spaceship-star_conflict_v.2/scene.gltf");
-
-	const meshRef = useRef();
-
-	console.log("pppp", position);
-	console.log("ssss", scale);
+export const SpaceshipModal = ({ changeModel, meshRef }) => {
+	const { scene } = useGLTF("./raven_spaceship-star_conflict_v.2/scene.gltf");
 
 	useEffect(() => {
-		console.log(meshRef);
 		// 초기 위치와 크기 설정
 		if (meshRef.current) {
-			meshRef.current.position.copy(new THREE.Vector3(position[0], position[1], position[2]));
-			meshRef.current.scale.copy(new THREE.Vector3(scale[0], scale[1], scale[2]));
+			meshRef.current.position.copy(new THREE.Vector3(0, 0, 0));
+			meshRef.current.scale.copy(new THREE.Vector3(0.004, 0.004, 0.004));
 		}
-	}, [position, scale]);
+	}, []);
 
 	useFrame(() => {
 		if (meshRef.current) {
-			const targetPosition = new THREE.Vector3(position[0], position[1], position[2]);
-			const targetScale = new THREE.Vector3(scale[0], scale[1], scale[2]);
-			meshRef.current.position.lerp(targetPosition, 0.01);
-			meshRef.current.scale.lerp(targetScale, 0.001);
+			const targetPosition = new THREE.Vector3(
+				changeModel.animate.position[0],
+				changeModel.animate.position[1],
+				changeModel.animate.position[2]
+			);
+			const targetScale = new THREE.Vector3(
+				changeModel.animate.scale[0],
+				changeModel.animate.scale[1],
+				changeModel.animate.scale[2]
+			);
+			meshRef.current.position.lerp(targetPosition, 0.05);
+			meshRef.current.scale.lerp(targetScale, 0.05);
 		}
 	});
 
 	return (
 		<group rotation={[Math.PI / 4, Math.PI / 2, 0]} ref={meshRef}>
-			<primitive object={spaceship.scene} />
+			<primitive object={scene} scale={[0.04, 0.04, 0.04]} />
 		</group>
 	);
 };
@@ -41,14 +43,16 @@ const SpaceshipModalCanvas = () => {
 	const [changeModel, setChangeModel] = useState({
 		animate: {
 			position: [0, 0, 0],
-			scale: [0.003, 0.003, 0.03],
+			scale: [0.001, 0.001, 0.01],
 		},
 	});
 
 	const [scrollCount, setScrollCount] = useState(0);
 	const maxScrollCount = 3;
 
+	const animationRef = useRef(null);
 	const scrollRef = useRef(null);
+	const meshRef = useRef(null);
 
 	useEffect(() => {
 		const handleScroll = (event) => {
@@ -62,12 +66,29 @@ const SpaceshipModalCanvas = () => {
 			}
 		};
 
-		let timeoutIn;
+		const animate = () => {
+			if (meshRef.current) {
+				const targetPosition = new THREE.Vector3(
+					changeModel.animate.position[0],
+					changeModel.animate.position[1],
+					changeModel.animate.position[2]
+				);
+				const targetScale = new THREE.Vector3(
+					changeModel.animate.scale[0],
+					changeModel.animate.scale[1],
+					changeModel.animate.scale[2]
+				);
+				meshRef.current.position.lerp(targetPosition, 0.05);
+				meshRef.current.scale.lerp(targetScale, 0.05);
+			}
+			animationRef.current = requestAnimationFrame(animate);
+		};
 
 		const handleDelayedScroll = (event) => {
 			event.preventDefault();
-			clearTimeout(timeoutIn);
-			timeoutIn = setTimeout(() => handleScroll(event), 500);
+			handleScroll(event);
+			cancelAnimationFrame(animationRef.current);
+			animationRef.current = requestAnimationFrame(animate);
 		};
 
 		if (scrollRef.current) {
@@ -77,7 +98,7 @@ const SpaceshipModalCanvas = () => {
 			if (scrollRef.current) {
 				scrollRef.current.removeEventListener("wheel", handleDelayedScroll);
 			}
-			clearTimeout(timeoutIn);
+			cancelAnimationFrame(animationRef.current);
 		};
 	}, []);
 
@@ -87,28 +108,28 @@ const SpaceshipModalCanvas = () => {
 			setChangeModel({
 				animate: {
 					position: [0, 0, 0],
-					scale: [0.003, 0.003, 0.003],
+					scale: [0.004, 0.004, 0.004],
 				},
 			});
 		} else if (scrollCount === 1) {
 			setChangeModel({
 				animate: {
-					position: [0, 0, 0],
-					scale: [0.001, 0.001, 0.001],
+					position: [5, 0, 0],
+					scale: [0.004, 0.004, 0.004],
 				},
 			});
 		} else if (scrollCount === 2) {
-			return setChangeModel({
+			setChangeModel({
 				animate: {
-					position: [0, 0, 0],
-					scale: [0.002, 0.002, 0.002],
+					position: [10, 0, 0],
+					scale: [0.004, 0.004, 0.004],
 				},
 			});
 		} else if (scrollCount === 3) {
-			return setChangeModel({
+			setChangeModel({
 				animate: {
-					position: [0, 0, 0],
-					scale: [0.003, 0.003, 0.003],
+					position: [15, 0, 0],
+					scale: [0.004, 0.004, 0.004],
 				},
 			});
 		}
@@ -124,14 +145,11 @@ const SpaceshipModalCanvas = () => {
 			ref={scrollRef}
 		>
 			<Suspense fallback={<Loader />}>
-				<OrbitControls enableZoom={false} />
+				<OrbitControls enableZoom={true} />
 				<ambientLight color="#CCCCCC" intensity={0.5} />
 
 				<directionalLight castShadow={false} />
-				<SpaceshipModal
-					scale={changeModel.animate.scale}
-					position={changeModel.animate.position}
-				/>
+				<SpaceshipModal changeModel={changeModel} meshRef={meshRef} />
 			</Suspense>
 		</Canvas>
 	);
